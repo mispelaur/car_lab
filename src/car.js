3277d3fa687a8d3c60b2e5835877a256a8a149ee
Alberto Ferioli <alberto_ferioli@yahoo.it>
2015-04-30 19:02:19 +0100

    done 8th task

--- src/car.js
+++ src/car.js
@@ -4,11 +4,15 @@ function Car(make, model, year, color, state, previousOwners, currentOwner, pass
   this.previousOwners = [];
   this.currentOwner = "Manufacturer";
   this.passengers = '';
+  this.newOwner = "Charlie";
 }
 
 Car.prototype.sale = function(newOwner){
   this.previousOwners.push(this.currentOwner);
 // i should move currentOwner to previousOwner[]
+
+  this.currentOwner = newOwner;
+// this function should update currentOwner with the new Owner
 };
 
 Car.prototype.paint = function(newColor){
