5697616c71265cd2e0b4ad92cd85bb9a390459ac
Daisy Molving <daisymolving@gmail.com>
2015-05-01 05:13:59 +1200

    moved currentOwner to previousOwner

--- src/car.js
+++ src/car.js
@@ -7,14 +7,16 @@ function Car(make, model, year, color){
 
 Car.prototype.state = "off";
 
-Car.prototype.previousOwners = "";
+Car.prototype.previousOwners = [];
 
 Car.prototype.currentOwner = "Manufacturer"
 
-Car.prototype.passengers = "";
+Car.prototype.passengers = [];
 
 Car.prototype.sale = function(newOwner){
 
+  this.previousOwners = ["Manufacturer"];
+  // Array.prototype.push(currentOwner, previousOwners)
 };
 
 Car.prototype.paint = function(newColor){
